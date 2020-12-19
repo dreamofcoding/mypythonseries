34dbd2ac18f70e32633ae7de4e475338a35d6f74
VIVEK <bigdreamcomes2020@gmail.com>
2020-12-19 16:01:18 +0530

    2 More lines added

--- firstgitfile.py
+++ firstgitfile.py
@@ -1,1 +1,9 @@
+
+# I Dragged/pulled this file from my github repository to in this Pycharm successfully.
+
 print("Hello World\n")
+
+# And now i am going to push this project/file to my github repository/after adding some more lines in it.
+
+print("AM GOING TO BUILD MY OWN PROJECTS IN PYTHON NOW")
+print("Say Good LUCK TO ME\n")
